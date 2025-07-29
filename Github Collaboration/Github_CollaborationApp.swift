dcaae0bf7ad1e981ee482eb7a6a0f1fbfeb205b9
Tanya Hingorani <tanyahingorani526@gmail.com>
2025-07-29 11:38:58 -0700

    Initial Commit

---
+++ Github Collaboration/Github_CollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_CollaborationApp.swift
+//  Github Collaboration
+//
+//  Created by ANTS on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Github_CollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
